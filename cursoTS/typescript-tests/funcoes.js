e29640089c07b2479ed9a545c36064457337eb61
guilhermepereiradev <93844722+guilhermeoxd@users.noreply.github.com>
2022-06-02 13:40:42 -0300

    primeiroProjetoJava

--- cursoTS/typescript-tests/funcoes.js
+++ cursoTS/typescript-tests/funcoes.js
@@ -17,5 +17,31 @@ function somar(...nums) {
     return nums.reduce((x, y) => x + y);
 }
 let total = somar(1, 2, 3, 4, 5, 6);
-let resultado = mostrarMensagem3('teste');
 console.log(total);
+const arr = [1, 2, 3, 4, 5, 6];
+// function forEach(array: any[], fn: (x: any) => void): void {
+//     for (let i of array){
+//         fn(i);
+//     }
+// }
+// forEach(arr, (x) => console.log(x * 2));
+// arr.forEach((x) => console.log(x*4));
+// function map(array: any[], fn: (x: any) => any): any{
+//     let novoArray: number[] = [];
+//     for (let i of array) {
+//             novoArray.push(fn(i));
+//     }
+//     return novoArray;
+// }
+// let novo = map(arr, (x) => (x**2));
+// console.log(novo)
+const names = ['Edu', 'Jajá', 'Luvannor'];
+function map(array, fn) {
+    let newNames = [];
+    for (const i of array) {
+        newNames.push(fn(i));
+    }
+    return newNames;
+}
+const newNames = map(names, (x) => x.length);
+console.log(newNames);
