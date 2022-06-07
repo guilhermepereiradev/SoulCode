62c9ce6dc2e0106eca8cd58ab731caa6e0cb3b56
guilhermepereiradev <93844722+guilhermeoxd@users.noreply.github.com>
2022-06-07 10:18:42 -0300

    segundo projeto angular

---
+++ cursoAngular/segundo-app/src/app/cartao/cartao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cartao', //criar uma tag chamada cartao <cartao></cartao>
+  templateUrl: './cartao.component.html',
+  styleUrls: ['./cartao.component.css']
+})
+export class CartaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
