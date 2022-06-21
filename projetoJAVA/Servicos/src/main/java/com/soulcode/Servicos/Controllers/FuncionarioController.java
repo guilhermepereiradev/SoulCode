06519fa81cbeb211ce77fca6d94239353f3e1bfb
guilhermepereiradev <guilhermepereira1609@hotmail.com>
2022-06-21 19:23:09 -0300

    Get e Post

--- projetoJAVA/Servicos/src/main/java/com/soulcode/Servicos/Controllers/FuncionarioController.java
+++ projetoJAVA/Servicos/src/main/java/com/soulcode/Servicos/Controllers/FuncionarioController.java
@@ -3,11 +3,11 @@ package com.soulcode.Servicos.Controllers;
 import com.soulcode.Servicos.Models.Funcionario;
 import com.soulcode.Servicos.Services.FuncionarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @CrossOrigin
@@ -23,4 +23,26 @@ public class FuncionarioController {
         List<Funcionario> funcionarios = funcionarioService.mostrarTodosFuncionarios();
         return funcionarios;
     }
+
+    @GetMapping("/funcionarios/{idFuncionario}")
+    public ResponseEntity<Funcionario> mostrarUmFuncionarioPeloId(@PathVariable Integer idFuncionario){
+        Funcionario funcionario = funcionarioService.mostrarUmFuncionarioPeloId(idFuncionario);
+        return ResponseEntity.ok().body(funcionario);
+    }
+
+    @GetMapping("/funcionariosEmail/{emailFuncioanario}")
+    public ResponseEntity<Funcionario> mostrarUmFuncioanarioPeloEmail(@PathVariable String emailFuncioanario){
+        Funcionario funcionario = funcionarioService.mostrarUmFuncioanarioPeloEmail(emailFuncioanario);
+        return ResponseEntity.ok().body(funcionario);
+    }
+
+    @PostMapping("/funcionarios")
+    public ResponseEntity<Funcionario> cadastrarFuncionario(@RequestBody Funcionario funcionario){
+//       Na linha 43 o funcionario ja é salvo na tabela
+//        agora precisamos criar uma uri para esse novo registro da tabela
+        funcionario = funcionarioService.cadatrarFuncionario(funcionario);
+        URI novaUri = ServletUriComponentsBuilder.fromCurrentRequest().path("id").buildAndExpand(funcionario.getIdFuncionario()).toUri();
+        return ResponseEntity.created(novaUri).body(funcionario);
+    }
+
 }
--- projetoJAVA/Servicos/src/main/java/com/soulcode/Servicos/Services/FuncionarioService.java
+++ projetoJAVA/Servicos/src/main/java/com/soulcode/Servicos/Services/FuncionarioService.java
@@ -5,7 +5,9 @@ import com.soulcode.Servicos.Repositories.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 // quando se fala em serviços, estamos falando dos metodos do CRUD da tabela
 @Service
@@ -19,5 +21,26 @@ public class FuncionarioService {
 
     public List<Funcionario> mostrarTodosFuncionarios(){
         return funcionarioRepository.findAll();
+    }
+
+    //vamos criar mais um serviço relacionado ao funcionário
+    //vamos criar um serviço de buscar o funcionario pelo seu id(chave primaria)
+
+    public Funcionario mostrarUmFuncionarioPeloId(Integer idFuncionario){
+        Optional<Funcionario> funcionario = funcionarioRepository.findById(idFuncionario);
+        return funcionario.orElseThrow();
+    }
+
+//    Vamos criar mais um serviço para buscar um funcionario pelo seu email
+    public Funcionario mostrarUmFuncioanarioPeloEmail(String email){
+        Optional<Funcionario> funcionario = funcionarioRepository.findByEmail(email);
+        return funcionario.orElseThrow();
+    }
+
+//  Vamos criar um serviço para cadastrar um novo funcionario
+    public Funcionario cadatrarFuncionario(Funcionario funcionario){
+//  So por preocação nos vamos colocar o id do funcinario como null
+        funcionario.setIdFuncionario(null);
+        return funcionarioRepository.save(funcionario);
     }
 }
